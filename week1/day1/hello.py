1c3bb28abd9ffa9375db6884b0ecf3c9164451b8
ShidoTachiki <taki1konokoetodoke@gmail.com>
2026-07-02 01:22:45 +0900

    Add Day1 hello program

---
+++ week1/day1/hello.py
@@ -0,0 +1,10 @@
+name = input("名前を入力してください: ")
+age = input("年齢を入力してください: ")
+food = input("好きな食べ物を入力してください: ")
+place = input("住んでいる場所を入力してください: ")
+
+print(f"こんにちは{name}さん！")
+print(f"年齢: {age}歳")
+print(f"好きな食べ物: {food}")
+print(f"住んでいる場所: {place}")
+
